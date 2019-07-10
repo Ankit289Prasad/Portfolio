2d81082c7cda2d2b86c589312c94d79486b93874
Ankit Prasad <ankit28prasad9@gmail.com>
2019-07-10 15:09:22 +0530

    Progress

--- Assets/Java Script/main.js
+++ Assets/Java Script/main.js
@@ -1,10 +1,11 @@
 let navbar=$(".navbar");
 
 $(window).scroll(function(){
-    let oTop = $(".section-2").offset().top-window.innerHeight;
+    let oTop=$(".section-2").offset().top-window.innerHeight;
     if($(window).scrollTop()>oTop){
         navbar.addClass("sticky");
-    }else{
+    }else
+    {
         navbar.removeClass("sticky");
-    }
+    } 
 });

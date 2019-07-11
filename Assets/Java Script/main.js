5561bb40f3a1b1083f8e490afb2e3a70d0ae1e9e
Ankit Prasad <ankit28prasad9@gmail.com>
2019-07-11 14:02:37 +0530

    Navbar-Animation

--- Assets/Java Script/main.js
+++ Assets/Java Script/main.js
@@ -1,7 +1,7 @@
 let navbar=$(".navbar");
 
 $(window).scroll(function(){
-    let oTop=$(".section-2").offset().top-window.innerHeight;
+    let oTop=$(".section-2").offset().top -window.innerHeight;
     if($(window).scrollTop()>oTop){
         navbar.addClass("sticky");
     }else
